afa562f7ea6b0932f8dc003961df925ee439109f
pritamkumarbishwas <pritamkumarbishwas85@gmail.com>
2024-06-15 00:10:11 +0530

    first commit

---
+++ src/ipConfig.js
@@ -0,0 +1,4 @@
+// let mockServer = {"endpoint": "https://xflix-backend-bicn.onrender.com"};
+// let mockServer = {"endpoint": "http://13.235.137.3:8082"};
+let mockServer = { "endpoint": "https://xflix-backend-492l.onrender.com" };
+export default mockServer;
